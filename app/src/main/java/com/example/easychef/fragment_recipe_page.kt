efead1bf0e82f8d03b58bd6fcfa2820de37aa96d
Landysh <zhabkakisa@gmail.com>
2021-07-07 21:34:26 +0300

    screens base

---
+++ app/src/main/java/com/example/easychef/fragment_recipe_page.kt
@@ -0,0 +1,11 @@
+package com.example.easychef
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fragment_recipe_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_activity_recipe_page)
+    }
+}
