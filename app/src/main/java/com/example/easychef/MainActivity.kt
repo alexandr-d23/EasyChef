3a238884f2362aa11d2ce89300b5bd03c846b85a
Elizav555 <78415957+Elizav555@users.noreply.github.com>
2021-07-10 19:36:42 +0300

    Навигация по регистрации и входу(Ура ура оно работает)

--- app/src/main/java/com/example/easychef/MainActivity.kt
+++ app/src/main/java/com/example/easychef/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.easychef
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -18,5 +19,16 @@ class MainActivity : AppCompatActivity() {
 
         val bnvMain = findViewById<BottomNavigationView>(R.id.nav_view)
         bnvMain.setupWithNavController(controller)
+        visibilityNavElements(controller)
+    }
+    private fun visibilityNavElements(navController: NavController) {
+        val bnvMain = findViewById<BottomNavigationView>(R.id.nav_view)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.authorizationFragment,
+                R.id.registrationFragment -> bnvMain?.visibility = View.GONE
+                else -> bnvMain?.visibility = View.VISIBLE
+            }
+        }
     }
 }
