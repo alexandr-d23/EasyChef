ba409a7a27affafdffbe662de91c14528010305a
Elizav555 <78415957+Elizav555@users.noreply.github.com>
2021-07-11 20:14:22 +0300

    не работает загрузка изображения

--- app/src/main/java/com/example/easychef/AddRecipeFragment.kt
+++ app/src/main/java/com/example/easychef/AddRecipeFragment.kt
@@ -5,55 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.example.easychef.databinding.FragmentAddRecipeBinding
+import com.example.easychef.databinding.FragmentRegistrationBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddRecipeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddRecipeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentAddRecipeBinding? = null
+    private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.buttonAddUrl.setOnClickListener {
+            if(binding.imageUrl.text.isNotBlank()) {
+                Glide.with(this).load(binding.imageUrl.text.toString()).into(binding.imageView)
+                binding.imageUrl.text.clear()
+            }
         }
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_recipe, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddRecipeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddRecipeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        _binding = FragmentAddRecipeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 }
